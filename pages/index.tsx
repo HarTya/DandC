e79020602e9278acf5d4d24b7e6a95d62e3bc655
XarTya <xartya@gmail.com>
2022-11-08 13:50:50 +0200

    next/image 13 update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { ReactElement } from 'react'
 import Layout from 'components/Layout'
 import type { NextPageWithLayout } from 'pages/_app'
 import styles from 'styles/Home.module.scss'
-import Image from 'next/image'
+import Image from 'next/legacy/image'
 
 const Home: NextPageWithLayout = () => {
   return (
@@ -13,6 +13,7 @@ const Home: NextPageWithLayout = () => {
       <Image
         alt='home-bg'
         src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg'
+        layout='fill'
         quality={100}
         className={styles.home_image}
       />
