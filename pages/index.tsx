ea49f73c38b53580eedb127b1c551044a662703e
XarTya <xartya@gmail.com>
2022-11-08 13:39:44 +0200

    mobile bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { ReactElement } from 'react'
 import Layout from 'components/Layout'
 import type { NextPageWithLayout } from 'pages/_app'
 import styles from 'styles/Home.module.scss'
+import Image from 'next/image'
 
 const Home: NextPageWithLayout = () => {
   return (
@@ -9,6 +10,14 @@ const Home: NextPageWithLayout = () => {
       <video autoPlay muted loop playsInline preload='metadata' poster='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg' className={styles.home_video}>
         <source src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.mp4' type='video/mp4' />
       </video>
+      <Image
+        alt='home-bg'
+        src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg'
+        layout='fill'
+        objectFit='cover'
+        quality={100}
+        className={styles.home_image}
+      />
     </div>
   )
 }
