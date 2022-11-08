e99a50ad40a0be0a84715c94ac3e036596c218ef
XarTya <xartya@gmail.com>
2022-11-09 01:20:05 +0200

    app animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
-import type { ReactElement } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import Layout from 'components/Layout'
 import type { NextPageWithLayout } from 'pages/_app'
 import styles from 'styles/Home.module.scss'
 import Image from 'next/legacy/image'
+import poster from 'public/images/poster.jpg'
 
 const Home: NextPageWithLayout = () => {
+
+  const [video, setVideo] = useState(false);
+
+  useEffect(() => {
+    const Debounce = setTimeout(() => {
+      setVideo(true)
+    }, 4000);
+
+    return () => clearTimeout(Debounce)
+  }, [])
+
   return (
     <div className={styles.home}>
-      <video autoPlay muted loop playsInline preload='metadata' poster='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg' className={styles.home_video}>
+      {video ? <video autoPlay muted loop playsInline preload='metadata' className={styles.home_video}>
         <source src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.mp4' type='video/mp4' />
-      </video>
+      </video> : <></>}
       <Image
-        alt='home-bg'
-        src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg'
+        alt='background'
+        src={poster}
         layout='fill'
         quality={100}
         className={styles.home_image}
