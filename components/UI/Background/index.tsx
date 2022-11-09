85b68e89efb4492706ed869f6e3243049d704321
XarTya <xartya@gmail.com>
2022-11-10 01:13:05 +0200

    background component

---
+++ components/UI/Background/index.tsx
@@ -0,0 +1,48 @@
+import styles from './Background.module.scss'
+import Image from 'next/legacy/image'
+import { useAppDispatch, useAppSelector } from 'store/hook'
+import { selectVideoState, setVideoState } from 'store/videoSlice'
+import { useEffect } from 'react'
+
+export default function Background() {
+
+    const videoState = useAppSelector(selectVideoState);
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        const Debounce = setTimeout(() => {
+        dispatch(setVideoState(true))
+        }, 4000)
+
+        return () => clearTimeout(Debounce)
+    }, [])
+
+    return (
+        <>
+            {videoState ? <div className={styles.video_background}></div> : <></>}
+            {videoState ? 
+                <video 
+                    autoPlay 
+                    muted 
+                    loop 
+                    playsInline 
+                    preload='metadata' 
+                    poster='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg' 
+                    className={styles.video}
+                >
+                    <source 
+                        src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.mp4' 
+                        type='video/mp4' 
+                    />
+                </video> 
+            : <></>}
+            <Image
+                alt='background'
+                src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg'
+                layout='fill'
+                quality={100}
+                className={styles.image}
+            />
+        </>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,13 @@
-import { ReactElement, useEffect, useState } from 'react'
+import { ReactElement } from 'react'
 import Layout from 'components/Layout'
 import type { NextPageWithLayout } from 'pages/_app'
 import styles from 'styles/Home.module.scss'
-import Image from 'next/legacy/image'
-import { useAppDispatch, useAppSelector } from 'store/hook'
-import { selectVideoState, setVideoState } from 'store/videoSlice'
+import Background from 'components/UI/Background'
 
 const Home: NextPageWithLayout = () => {
-
-  const videoState = useAppSelector(selectVideoState);
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    const Debounce = setTimeout(() => {
-      dispatch(setVideoState(true))
-    }, 4000)
-
-    return () => clearTimeout(Debounce)
-  }, [])
-
   return (
     <div className={styles.home}>
-      {videoState ? <video autoPlay muted loop playsInline preload='metadata' poster='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg' className={styles.home_video}>
-        <source src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.mp4' type='video/mp4' />
-      </video> : <></>}
-      <Image
-        alt='background'
-        src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg'
-        layout='fill'
-        quality={100}
-        className={styles.home_image}
-      />
+      <Background />
       <div id='page-content'>
         Main
       </div>
