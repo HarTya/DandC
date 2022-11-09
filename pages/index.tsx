650acaf30f379e9daf6399a096d9d1fb8221ce1a
XarTya <xartya@gmail.com>
2022-11-09 17:46:04 +0200

    video and image fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from 'components/Layout'
 import type { NextPageWithLayout } from 'pages/_app'
 import styles from 'styles/Home.module.scss'
 import Image from 'next/legacy/image'
-import poster from 'public/images/poster.jpg'
+// import poster from 'public/images/poster.jpg'
 
 const Home: NextPageWithLayout = () => {
 
@@ -19,12 +19,12 @@ const Home: NextPageWithLayout = () => {
 
   return (
     <div className={styles.home}>
-      {video ? <video autoPlay muted loop playsInline preload='metadata' className={styles.home_video}>
+      {video ? <video autoPlay muted loop playsInline preload='metadata' poster='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg' className={styles.home_video}>
         <source src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.mp4' type='video/mp4' />
       </video> : <></>}
       <Image
         alt='background'
-        src={poster}
+        src='https://res.cloudinary.com/dandcready/video/upload/q_100/v1667904954/dandcreadywebsite/fj7x0cqmojwe8jfvnr3l.jpg'
         layout='fill'
         quality={100}
         className={styles.home_image}
