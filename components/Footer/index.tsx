1bee51ad5e2c38819c4cbf5c4f08ef7d49b9849d
XarTya <xartya@gmail.com>
2022-11-18 20:39:29 +0200

    header and footer effect

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,4 +1,12 @@
+import { useEffect } from 'react'
+
 export default function Footer() {
+
+    useEffect(() => {
+        console.log('Footer mounted...')
+        return () => console.log('Footer unmounted...')
+    }, [])
+
     return (
         <footer></footer>
     )
